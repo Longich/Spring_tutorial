d460afa2f2f33cadc74f2f5af015444db905df2e
Longich <ka.kazu.kazuki@gmail.com>
2017-09-06 03:21:01 +0900

    ユーザ認証実装

--- src/main/java/mrs/app/reservation/ReservationController.java
+++ src/main/java/mrs/app/reservation/ReservationController.java
@@ -4,10 +4,12 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,6 +29,7 @@ import mrs.domain.service.reservation.AlreadyReservedException;
 import mrs.domain.service.reservation.ReservationService;
 import mrs.domain.service.reservation.UnavailableReservationException;
 import mrs.domain.service.room.RoomService;
+import mrs.domain.service.user.ReservationUserDetails;
 
 @Controller
 @RequestMapping("reservations/{date}/{roomId}")
@@ -50,16 +53,57 @@ public class ReservationController {
             @PathVariable("roomId") Integer roomId, Model model) {
         ReservableRoomId reservableRoomId = new ReservableRoomId(roomId, date);
         List<Reservation> reservations = reservationService.findReservations(reservableRoomId);
-        
-        List<LocalTime> timeList = Stream.iterate(LocalTime.of(0, 0), t -> t.plusMinutes(30))
-                                         .limit(24 * 2)
-                                         .collect(Collectors.toList());
+        LocalTime baseTime = LocalTime.of(0, 0);
+        List<LocalTime> timeList = IntStream.range(0, 24 * 2)
+                .mapToObj(i -> baseTime.plusMinutes(30 * i)).collect(Collectors.toList());
         
         model.addAttribute("room", roomService.findMeetingRoom(roomId));
         model.addAttribute("reservations", reservations);
         model.addAttribute("timeList", timeList);
-        model.addAttribute("user", dummyUser());
         return "reservation/reserveForm";
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    String reserve(@Validated ReservationForm form, BindingResult bindingResult,
+            @AuthenticationPrincipal ReservationUserDetails userDetails,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
+            @PathVariable("roomId") Integer roomId, Model model) {
+        if (bindingResult.hasErrors()) {
+            return reserveForm(date, roomId, model);
+        }
+        
+        ReservableRoom reservableRoom = new ReservableRoom(new ReservableRoomId(roomId, date));
+        Reservation reservation = new Reservation();
+        reservation.setStartTime(form.getStartTime());
+        reservation.setEndTime(form.getEndTime());
+        reservation.setReservableRoom(reservableRoom);
+        reservation.setUser(userDetails.getUser());
+        
+        try {
+            reservationService.reserve(reservation);
+        } catch (UnavailableReservationException | AlreadyReservedException e) {
+            model.addAttribute("error", e.getMessage());
+            return reserveForm(date, roomId, model);
+        }
+        return "redirect:/reservations/{date}/{roomId}";
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, params = "cancel")
+    String cancel(@AuthenticationPrincipal ReservationUserDetails userDetails,
+            @RequestParam("reservationId") Integer reservationId,
+            @PathVariable("roomId") Integer roomId,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") LocalDate date,
+            Model model) {
+        
+        User user = userDetails.getUser();
+        try {
+            reservationService.cancel(reservationId, user);
+        } catch (AccessDeniedException e) {
+            model.addAttribute("error", e.getMessage());
+            return reserveForm(date, roomId, model);
+        }
+        
+        return "redirect:/reservations/{date}/{roomId}";
     }
     
     private User dummyUser() {
@@ -104,7 +148,7 @@ public class ReservationController {
         User user = dummyUser();
         try {
             reservationService.cancel(reservationId, user);
-        } catch (IllegalStateException e) {
+        } catch (AccessDeniedException e) {
             model.addAttribute("error", e.getMessage());
             return reserveForm(date, roomId, model);
         }
